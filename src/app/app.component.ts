ed01b9d6290657cc0d6708da2de2dd1cb70f21c0
Alvaro Junqueira <alvarocjunq@gmail.com>
2018-10-18 17:08:57 +0100

    remove domain

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   ngOnInit(): void {
-    document.domain = 'herokuapp.com';
+    // document.domain = 'herokuapp.com';
     window.addEventListener('message', receiveMessage, false);
     function receiveMessage(event) {
       console.log(event);
