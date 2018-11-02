413667682b60f09570cc9cbe825a1876e08df744
Alvaro Junqueira <alvarocjunq@gmail.com>
2018-11-02 12:17:02 +0000

    fix name folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const ROUTES: Routes = [
     { path: 'home', loadChildren: './home/home.module#HomeModule' },
-    { path: 'detail', loadChildren: './.lazy-modules/project-detail.module#ProjectDetailModule' },
+    { path: 'detail', loadChildren: './lazy-modules/project-detail.module#ProjectDetailModule' },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', redirectTo: '/home', pathMatch: 'full' },
 ];
--- src/app/lazy-modules/project-detail.module.ts
+++ src/app/lazy-modules/project-detail.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MaterialModule } from '../material-module';
 import { RouterModule, Routes } from '@angular/router';
+
+import { MaterialModule } from '../material-module';
 
 import { ScaffoldingDetailModule, ScaffoldingDetailComponent } from 'scaffolding-detail';
 
